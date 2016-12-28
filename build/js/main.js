e9a767541cc70cdb35fc065422ec38d3139787ff
rr235 <rash235r@hotmail.com>
2016-12-28 04:50:20 +0400

    Added Mobile Responsiveness

--- build/js/main.js
+++ build/js/main.js
@@ -364,6 +364,16 @@ angular.module('mainApp')
         $scope.isActive = function (path) {
             return $location.path() === path;
         }
+        $scope.showMobileMenu = false;
+
+        $scope.toggleMobileMenu = function () {
+            $scope.showMobileMenu = !$scope.showMobileMenu;
+        }
+
+        $scope.goTo = function(path) {
+            $scope.showMobileMenu = false;
+            $location.path(path);
+        }
     }]);
 angular.module('mainApp')
     .controller('registerCtrl', ['$scope', '$location', '$anchorScroll', '$window', 'RegisterModel',
--- src/js/controllers/menuCtrl.js
+++ src/js/controllers/menuCtrl.js
@@ -3,4 +3,14 @@
         $scope.isActive = function (path) {
             return $location.path() === path;
         }
+        $scope.showMobileMenu = false;
+
+        $scope.toggleMobileMenu = function () {
+            $scope.showMobileMenu = !$scope.showMobileMenu;
+        }
+
+        $scope.goTo = function(path) {
+            $scope.showMobileMenu = false;
+            $location.path(path);
+        }
     }]);
